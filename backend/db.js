f48d7e63efe54d0cd729f27699efb6ff62452f42
Ujjawal Kumar Gupta <99381477+Ukgrocks@users.noreply.github.com>
2025-04-13 15:36:22 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import moongoose from 'mongoose';
 const mongoURI = "mongodb+srv://ukgprojects:aXetHNTPVWRCkMrU@cluster0.gffxi5g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-"
+
 const connectToMongo = ()=>{
     moongoose.connect(mongoURI);
 }
